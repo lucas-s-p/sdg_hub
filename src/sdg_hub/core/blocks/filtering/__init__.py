770ea76d3e5cc6a091e5cb2cb549d2ac35f7c895
lucas-s-p <lucas.pereira@ccc.ufcg.edu.br>
2026-08-19 14:58:44 -0300

    .

--- src/sdg_hub/core/blocks/filtering/__init__.py
+++ src/sdg_hub/core/blocks/filtering/__init__.py
@@ -6,6 +6,7 @@ This module provides blocks for filtering datasets based on various criteria.
 
 # Local
 from .column_value_filter import ColumnValueFilterBlock
+from .java_compiler_block import JavaCompilerBlock
 
 __all__ = [
     "ColumnValueFilterBlock",
